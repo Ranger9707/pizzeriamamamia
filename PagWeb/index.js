0be0ca3a0d5270a17ac5f00132dcfe488b61b296
Matías Vespa <30608973+Ranger9707@users.noreply.github.com>
2024-09-03 19:38:45 -0300

    Add files via upload

---
+++ PagWeb/index.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", () =>{
+
+const reservaBtn = document.getElementById("reservaBtn");
+
+function handleClick() {
+window.alert("Gracias por reservar.");
+}
+
+reservaBtn.addEventListener("click", handleClick);
+
+});
